91ed18e13f659481f5afa34796a50b12fae27745
ekivolowitz <ekivolowitz@wisc.edu>
2017-01-17 19:13:52 -0600

    Nothing really important here.

--- CPP/Game.cpp
+++ CPP/Game.cpp
@@ -41,17 +41,48 @@ bool checkBeginGameInput(string input)
 }
 
 
+
+/**
+*	This function is used in main to get a random word to create the game on. 
+*	@param randomNumber - integer between 1 and 854 (NUMBER_WORDS_IN_HANGMAN_FILE)/
+*	@return String - random word from file on line number randomNumber.
+*	@return NULL if for some reason randomNumber was out of range 1 -> 854
+*/
+string getRandomWord(int randomNumber)
+{
+	ifstream myFile;
+	myFile.open("hangman_words.txt");
+	for(int i = 0; i < NUMBER_WORDS_IN_HANGMAN_FILE; i++)
+	{
+		if(i == randomNumber)
+		{
+			string retString;
+			getline(myFile, retString);
+			return retString;
+		}
+		else
+		{
+			string retString;
+			getline(myFile, retString);
+		}
+	}
+	return NULL;
+}
+
+/**
+*
+*	Main point of execution. 
+*
+*/
 int main()
 {
-	Environment env;
+	
 	srand(time(NULL));
 	int randomNumber = rand() % NUMBER_WORDS_IN_HANGMAN_FILE;
-
 	string secretWord = getRandomWord(randomNumber);
-	cout << "Welcome to Hangman. This program was brought to you by Evan Kivolowitz as a way to learn C++." << endl;
-	env.toString();
+	Environment env = new Environment(secretWord);
+	cout << "Welcome to Hangman. This program was brought to you by Evan Kivolowitz as a way to learn C++.\n" <<  env.toString() << endl;
 
-	
 	while(true)
 	{
 		cout << "Do you want to play? \n(Y or N): ";
@@ -59,5 +90,7 @@ int main()
 		cin >> play;
 		if(checkBeginGameInput(play)) break;	
 	}
+	cout << env.toString();
 
+	free(env);
 }
