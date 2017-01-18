370e8699b1a07cea0dab691d13680c4c7726ba16
ekivolowitz <ekivolowitz@wisc.edu>
2017-01-17 19:14:29 -0600

    Cleaning up files.

---
+++ Environment.cpp
@@ -0,0 +1,46 @@
+#include "Environment.h"
+
+Environment::Environment()
+{
+
+//Head is drawn at 6th line
+//Torso (line 7 and 8) and arms are drawn at 7th line.
+//Legs drawn on 9th line
+//Guessed letters go on line 14
+//Answers go on line 17
+	env = "/////////////////////////////////////////////////////\n\
+//\n\
+//\n\
+//		///////////////\n\
+//		//       | \n\
+//		//       O \n\
+//		//      /|\\\n\
+//		//       |\n\
+//		//      / \\\n\
+//		//\n\
+//		/////////////////\n\
+/////////////////////////////////////////////////////\n\
+//\n\
+//	Guessed Letters:\n\
+//\n\
+//\n\
+//	Answer:\n\
+//\n\
+/////////////////////////////////////////////////////\n";
+}
+
+string Environment::getEnvironment()
+{
+	return this->env;
+}
+
+
+
+- id: leon
+  name: Leon Dinh
+  year: Senior
+  major: Computer Sciences
+  github: https://github.com/lwdinh
+  photo: ldinh.jpg
+  emoji: art
+l
